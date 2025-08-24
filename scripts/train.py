cfb03cfb75ddb2c6fe353ed86c5d5aa0bea23133
Kumar Kartikay <krkartikay@gmail.com>
2025-08-24 17:27:33 +0530

    set bs=512 and increase lr

--- scripts/train.py
+++ scripts/train.py
@@ -55,8 +55,8 @@ def get_next_model_path(out_dir="out"):
 
 def main():
     # Hyperparameters
-    batch_size = 16
-    lr = 3e-4
+    batch_size = 512
+    lr = 1e-3
     l2_weight = 1e-4  # L2 regularization weight
     min_improvement = 0.0001  # Minimum improvement to continue training (0.01%)
 
