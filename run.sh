fac1d93e764441dc524e6eebf02b3cea8f89c06a
Kumar Kartikay <krkartikay@gmail.com>
2025-08-24 17:38:01 +0530

    upd

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@
 
 while true; do
   python3 scripts/run_all.py 2>&1 | tee -a run_all.log
+  dataset_size=$(python3 -c "from scripts.dataset import TrainingDataset; print(len(TrainingDataset('training_data.bin')))")
+  echo -e "\n\nCurrent TrainingDataset size: $dataset_size\n\n" | tee -a run_all.log
   sleep 1
 done
