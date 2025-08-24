4647da4bb6d605566ff1fe9ea1f96755f0517eec
Kumar Kartikay <krkartikay@gmail.com>
2025-08-24 17:41:14 +0530

    unbuffer output

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # with a short delay to avoid a tight loop
 
 while true; do
-  python3 scripts/run_all.py 2>&1 | tee -a run_all.log
+  python3 -u scripts/run_all.py 2>&1 | tee -a run_all.log
   dataset_size=$(python3 -c "from scripts.dataset import TrainingDataset; print(len(TrainingDataset('training_data.bin')))")
   echo -e "\n\nCurrent TrainingDataset size: $dataset_size\n\n" | tee -a run_all.log
   sleep 1
